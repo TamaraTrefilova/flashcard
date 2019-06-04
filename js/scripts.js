b7abd2e6cc16edaf5a5aac5e5b061ce0679928b4
Rochelle Roberts and Tamara Trefilova <tamara@>
2019-06-04 14:24:34 -0700

    flash cards working

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+$(function(){
+  $(".clickable").click(function() {
+    $("#javascript").toggle();
+  });
+
+  $(".clickable1").click(function() {
+    $("#operators").toggle();
+  });
+
+  $(".clickable2").click(function() {
+    $("#variables").toggle();
+  });
+  $(".clickable3").click(function() {
+    $("#naming").toggle();
+  });
+  $(".clickable4").click(function() {
+    $("#functions").toggle();
+  });
+  $(".clickable5").click(function() {
+    $("#arguments").toggle();
+  });
+  $(".clickable6").click(function() {
+    $("#parameters").toggle();
+  });
+  $(".clickable7").click(function() {
+    $("#return").toggle();
+  });
+  $(".clickable8").click(function() {
+    $("#chaining").toggle();
+  });
+  $(".clickable9").click(function() {
+    $("#strings").toggle();
+  });
+  $(".clickable10").click(function() {
+    $("#boolean").toggle();
+  });
+
+
+});
